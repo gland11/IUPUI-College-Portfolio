185660f6a383612612ec268f428b362c937538e2
gland11 <114028608+gland11@users.noreply.github.com>
2023-03-25 11:49:42 -0400

    Add files via upload

---
+++ Lab_07/LandisLab07_164/LandisLab07_164/CalcPresentValue.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// George Landis
+// ECET 164
+// Lab_07 Present Value
+// 2/21/2018
+namespace LandisLab07_164
+{
+    public partial class CalcPresentValue : Form
+    {
+        public CalcPresentValue()
+        {
+            InitializeComponent();
+        }
+
+        private double PresentValue(double num1, double num2, double num3)
+        {
+            double result = 0;
+            result = (num1 / (Math.Pow (1+(num2 / 100), num3)));
+            return result;
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            // Three Var all doubles
+            double valueWanted = 0;
+            double interestRateWanted = 0;
+            double numberYearsWanted = 0;
+            double depositToday = 0;
+
+            //Validate user input 
+            if(double.TryParse(valueWantedTextBox.Text,out valueWanted))
+            {
+                if(double.TryParse(rateWantedTextBox.Text,out interestRateWanted))
+                {
+                    if(double.TryParse(yearsWantedTextBox.Text,out numberYearsWanted))
+                    {
+                        // Call Method
+                        depositToday = PresentValue(valueWanted, interestRateWanted, numberYearsWanted);
+
+                        //Display that deposit
+                        OutputLabel.Text = depositToday.ToString("n2");
+                    }
+                    //else for yearsWanted
+                    else
+                    {
+                        MessageBox.Show("Enter a Positive Number for Number of Years Wanted.");
+                        yearsWantedTextBox.Focus();
+                    }
+                }
+                //else for rateWanted
+                else
+                {
+                    MessageBox.Show("Enter a Positive Number for Annual Interest Rate Wanted.");
+                    rateWantedTextBox.Focus();
+                }
+            }
+            // Else for ValueWanted
+            else
+            {
+                MessageBox.Show("Enter a Positive Number for the Future Value Wanted");
+                valueWantedTextBox.Focus();
+            }
+        }
+
+        private void clrBtn_Click(object sender, EventArgs e)
+        {
+            valueWantedTextBox.Text = "";
+            rateWantedTextBox.Text = "";
+            yearsWantedTextBox.Text = "";
+            OutputLabel.Text = "";
+        }
+
+        private void extBtn_Click(object sender, EventArgs e)
+        {
+            // Close Program
+            Close();
+        }
+    }
+}
---
+++ Lab_08/LandisLab08_164/LandisLab08_164/DisplaySales.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace LandisLab08_164
+{
+    public partial class DisplaySales : Form
+    {
+        public DisplaySales()
+        {
+            InitializeComponent();
+        }
+        //method lowest
+        private double GetLowest(double[] lArray)
+        {
+            double low = lArray[0];
+           
+            for(int index = 1; index<lArray.Length; index++)
+            {
+                if(lArray[index]< low)
+                {
+                    low = lArray[index];
+                }
+            }
+             
+            return low;
+        }
+        //method Highest 
+        private double GetHighest(double[] hArray)
+        {
+            double high = 0;
+           
+            for(int index = 1; index<hArray.Length; index++)
+            {
+                if(hArray[index]> high)
+                {
+                    high = hArray[index];
+                }
+            }
+            
+            return high;
+        }
+        // method GetAverage
+        private double GetAverage(double[] avegArray)
+        {
+            double total = 0;
+            double myAverage = 0;
+            for(int count = 0; count<avegArray.Length; count++)
+            {
+                total += avegArray[count];
+            }
+            myAverage = total / avegArray.Length;
+            return myAverage;
+        }
+        private void ProcessSalesFile()
+        {
+            //Declare some variables
+            StreamReader inputFile;
+            const int SIZE = 7;
+            double[] myArray = new double [SIZE];
+            try
+            {
+                // Var
+                // 3 more variables 
+                // Average, Highest, and lowest
+                 double average = 0;
+                 double highest = 0;
+                 double lowest = 0;
+
+                //part 2
+                double sum = 0;
+                int index = 0;
+                //open the file and get a streamReader object
+                inputFile = File.OpenText("Sales.txt");
+                // Clear listbox
+                salesListBox.Items.Clear();
+
+                // Read the file's contents
+                while (!inputFile.EndOfStream && index < myArray.Length)
+                {
+                    myArray[index] = double.Parse(inputFile.ReadLine());
+                    //sum all Numbers
+                    sum += myArray[index];
+                    
+                    //Add the number to ListBox
+                    salesListBox.Items.Add(myArray[index].ToString("c"));
+                    index++;
+                }
+                // Close file
+                inputFile.Close();
+
+                // Display
+                totalLabel.Text = sum.ToString("c");
+
+                average = GetAverage(myArray);
+
+                avegLabel.Text = average.ToString("c");
+
+                highest = GetHighest(myArray);
+
+                highestLabel.Text = highest.ToString("c");
+
+                lowest = GetLowest(myArray);
+
+                lowLabel.Text = lowest.ToString("c");
+
+
+            }
+            catch (Exception ex)
+            {
+                //Message to user
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void extBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        
+        private void writeTxtBtn_Click(object sender, EventArgs e)
+        {
+            ProcessSalesFile(); 
+        }
+
+        private void clrBtn_Click(object sender, EventArgs e)
+        {
+            salesListBox.Items.Clear();
+            totalLabel.Text = "";
+            avegLabel.Text = "";
+            highestLabel.Text = "";
+            lowLabel.Text = "";
+        }
+    }
+}
