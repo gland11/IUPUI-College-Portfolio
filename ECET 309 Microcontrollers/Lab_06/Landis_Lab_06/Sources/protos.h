55ea505e79d27194fe107ebf66203f3ce218913d
gland11 <114028608+gland11@users.noreply.github.com>
2023-03-26 16:29:13 -0400

    Add files via upload

---
+++ ECET 309 Microcontrollers/Lab_06/Landis_Lab_06/Sources/protos.h
@@ -0,0 +1,5 @@
+void Delay(unsigned long int delayVal);
+void InitializeLCD(void);
+void Display(unsigned char* message);
+void LCDInit(unsigned char dataVal);
+void LCDData(unsigned char stringVal);
---
+++ ECET 309 Microcontrollers/Lab_07/Landis_Lab_07_30903/Sources/protos.h
@@ -0,0 +1,7 @@
+void Delay(unsigned long int);
+void InitializeLCD(void);
+void Display(unsigned char*);
+void LCDInit(unsigned char);
+void LCDData(unsigned char);
+void BusyCheck(void);
+void CallingFunction(void);
